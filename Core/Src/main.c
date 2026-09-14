f3a16d8b025c0a4146d93f4837a5eb0ab26eb494
Eufrasio Arruda Neto <eufrasioarrudaneto@gmail.com>
2026-09-14 14:17:21 -0300

    questoes da atividade avaliativa

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -129,6 +129,78 @@ void aula_11_09(){
 
 }
 
+
+void questao1(){
+	while(1){
+		GPIO_Toggle_Pin(GPIOA, PIN_6);
+		Delay_ms(250);
+		GPIO_Toggle_Pin(GPIOA, PIN_6);
+		Delay_ms(250);
+	}
+
+}
+
+void questao3(){
+	while(1){
+		for(int i = 0; i < 4; i++){
+			GPIO_Toggle_Pin(GPIOA, PIN_6);
+			Delay_ms(50);
+			GPIO_Toggle_Pin(GPIOA, PIN_6);
+			Delay_ms(50);
+		}
+		Delay_ms(400);
+	}
+}
+
+void questao5(){
+	for(int i = 0; i < 3;i++){
+		GPIOA->BSRR = i;
+	}
+}
+
+void questao7(){
+	for(int i = 0; i < 8;i++){
+		GPIOA->BSRR = i;
+	}
+}
+
+void questao9(){
+
+	int sinal = 0;
+
+	while(1){
+		if(sinal == 0){
+			GPIO_Toggle_Pin(GPIOA, PIN_6);
+			Delay_ms(3000);
+			GPIO_Toggle_Pin(GPIOA, PIN_6);
+			GPIO_Toggle_Pin(GPIOA, PIN_7);
+			Delay_ms(2000);
+			GPIO_Toggle_Pin(GPIOA, PIN_7);
+			GPIO_Toggle_Pin(GPIOA, PIN_8);
+			Delay_ms(5000);
+			GPIO_Toggle_Pin(GPIOA, PIN_8);
+			Delay_ms(1000);
+			sinal++;
+		}else{
+			GPIO_Toggle_Pin(GPIOA, PIN_6);
+			Delay_ms(3000);
+			GPIO_Toggle_Pin(GPIOA, PIN_6);
+			GPIO_Toggle_Pin(GPIOA, PIN_7);
+			Delay_ms(2000);
+			GPIO_Toggle_Pin(GPIOA, PIN_7);
+			GPIO_Toggle_Pin(GPIOA, PIN_8);
+			Delay_ms(5000);
+			GPIO_Toggle_Pin(GPIOA, PIN_8);
+			Delay_ms(1000);
+			sinal--;
+		}
+	}
+}
+
+void questao11(){
+
+}
+
 void rotina(){
 	for(int i = 0; i < 5;i++){
 		GPIO_Toggle_Pin(GPIOA, PIN_6);
